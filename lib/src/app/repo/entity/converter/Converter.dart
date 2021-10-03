908f5f9f530511b5931d7016e9562f76a3eadbe6
Jesus Hdez <jhernandezb96@gmail.com>
2021-10-02 22:35:32 -0500

    Documentation of /converter package

--- lib/src/app/repo/entity/converter/Converter.dart
+++ lib/src/app/repo/entity/converter/Converter.dart
@@ -1,5 +1,8 @@
 import 'package:clean_core/clean_core.dart';
 
+/// Mixin to implement by an Entity in order to convert it to it's correspondent domain.
+/// Generally NOT implemented directly, instead extends the entity from [BasicEntityObject].
 mixin Converter<Domain extends BasicDomainObject> {
+  ///Convert the current entity to it's correspondent domain.
   Domain toDomain();
 }
--- lib/src/app/repo/entity/converter/DefaultGeneralConverter.dart
+++ lib/src/app/repo/entity/converter/DefaultGeneralConverter.dart
@@ -1,22 +1,29 @@
 import 'package:clean_core/clean_core.dart';
 
+///Default implementation of [GeneralConverter]
 class DefaultGeneralConverter<Domain extends BasicDomainObject,
         Entity extends BasicEntityObject<Domain>>
     extends GeneralConverter<Domain, Entity> {
+  ///By default calling Entity.toDomain()
   @override
   Domain toDomain(Entity entity) {
     return entity.toDomain();
   }
 
+  ///By default ....
   @override
   Entity toEntity(Domain domain) {
-    throw UnimplementedError("no se ha implementado"); //todo: implementar
+    throw UnimplementedError("no se ha implementado"); //todo: implement
   }
 
+  ///By default converting one by one into a new list
+  ///using the single toDomain(entity) converter.
   @override
   List<Domain> toDomainAll(List<Entity> entities) =>
       entities.map((entity) => this.toDomain(entity)).toList();
 
+  ///By default converting one by one into a new list
+  ///using the single toEntity(domain) converter.
   @override
   List<Entity> toEntityAll(List<Domain> domains) =>
       domains.map((domain) => this.toEntity(domain)).toList();
