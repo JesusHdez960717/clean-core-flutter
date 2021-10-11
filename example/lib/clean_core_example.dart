3f326400fcb36a693a3ca1dbe46f5b9d037e9487
Jesus Hdez <jhernandezb96@gmail.com>
2021-10-11 16:44:01 -0500

    Example: Rename core => app

--- example/lib/clean_core_example.dart
+++ example/lib/clean_core_example.dart
@@ -1,17 +1,17 @@
 library cleancore_exporter;
 
 //domain
-export 'core/domain/CleanCoreExampleDomainExporter.dart';
+export 'app/domain/CleanCoreExampleDomainExporter.dart';
 
 //use case
-export 'core/usecase_def/CleanCoreExampleUseCaseExporter.dart';
-export 'core/usecase_impl/CleanCoreExampleUseCaseImplExporter.dart';
+export 'app/usecase_def/CleanCoreExampleUseCaseExporter.dart';
+export 'app/usecase_impl/CleanCoreExampleUseCaseImplExporter.dart';
 
 //entity
 export 'repo/entities/Entities.dart';
 
 //repo
-export 'core/repo_def/CleanCoreExampleRepoDefExporter.dart';
+export 'app/repo_def/CleanCoreExampleRepoDefExporter.dart';
 export 'repo/repo_impl/RepoImplExporter.dart';
 
 //repo internal
@@ -19,5 +19,5 @@ export 'repo/repo_internal/CleanCoreExampleRepoInternalExporter.dart';
 export 'repo/repo_internal_impl/CleanCoreExampleRepoInternalImplExporter.dart';
 
 //module
-export 'core/module/CleanCoreExampleCoreModule.dart';
+export 'app/module/CleanCoreExampleCoreModule.dart';
 export 'repo/module/CleanCoreExampleRepoModule.dart';
