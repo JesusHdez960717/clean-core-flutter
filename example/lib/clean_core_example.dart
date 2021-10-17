d75779ee7d616fbbb8f1521c11d5793742bac696
Jesus Hdez <jhernandezb96@gmail.com>
2021-10-17 17:28:39 -0500

    +layer exporter
    Added exporter for the app and repo layer

--- example/lib/clean_core_example.dart
+++ example/lib/clean_core_example.dart
@@ -1,23 +1,5 @@
 library cleancore_exporter;
 
-//domain
-export 'app/domain/CleanCoreExampleDomainExporter.dart';
+export 'app/clean_core_examaple_app_exporter.dart';
 
-//use case
-export 'app/usecase_def/CleanCoreExampleUseCaseExporter.dart';
-export 'app/usecase_impl/CleanCoreExampleUseCaseImplExporter.dart';
-
-//entity
-export 'repo/entities/Entities.dart';
-
-//repo
-export 'app/repo_def/CleanCoreExampleRepoDefExporter.dart';
-export 'repo/repo_impl/RepoImplExporter.dart';
-
-//repo External
-export 'repo/repo_external/CleanCoreExampleRepoInternalExporter.dart';
-export 'repo/repo_external_impl/CleanCoreExampleRepoInternalImplExporter.dart';
-
-//module
-export 'app/module/CleanCoreExampleCoreModule.dart';
-export 'repo/module/CleanCoreExampleRepoModule.dart';
+export 'repo/clean_core_examaple_repo_exporter.dart';
