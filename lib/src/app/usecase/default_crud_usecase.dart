188c71a4158e5e247fa5b8c8fb4d1e718a4fd8c9
JHB <jhernandezb96@gmail.com>
2022-02-06 20:54:35 -0500

    app/usecase/*: -print to default use cases

--- lib/src/app/usecase/default_crud_usecase.dart
+++ lib/src/app/usecase/default_crud_usecase.dart
@@ -29,51 +29,26 @@ abstract class DefaultCRUDUseCase<Domain extends BasicDomainObject>
   DefaultCRUDUseCase({required this.repo});
 
   @override
-  Domain create(Domain newObject) {
-    print("${PropertyChangeConstrains.BEFORE_CREATE}  => $newObject");
-    newObject = repo.create(newObject);
-    print("${PropertyChangeConstrains.AFTER_CREATE}  => $newObject");
-    return newObject;
-  }
+  Domain create(Domain newObject) => repo.create(newObject);
 
   @override
-  Domain edit(Domain objectToEdit) {
-    print("${PropertyChangeConstrains.BEFORE_EDIT}  => $objectToEdit");
-    Domain domain = repo.edit(objectToEdit);
-    print("${PropertyChangeConstrains.AFTER_EDIT}  => $objectToEdit");
-    return domain;
-  }
+  Domain edit(Domain objectToEdit) => repo.edit(objectToEdit);
 
   @override
-  List<Domain> findAll() {
-    print("${PropertyChangeConstrains.BEFORE_FIND_ALL}  => EmptyList{}");
-    List<Domain> list = repo.findAll();
-    print("${PropertyChangeConstrains.AFTER_FIND_ALL}  => $list");
-    return list;
-  }
+  List<Domain> findAll() => repo.findAll();
 
   @override
-  Domain findBy(int keyId) {
-    print("${PropertyChangeConstrains.BEFORE_FIND_BY}  => $keyId");
-    Domain object = repo.findBy(keyId);
-    print("${PropertyChangeConstrains.AFTER_FIND_BY}  => $keyId");
-    return object;
-  }
+  Domain findBy(int keyId) => repo.findBy(keyId);
 
   @override
-  Domain destroy(Domain objectToDestroy) {
-    print("${PropertyChangeConstrains.BEFORE_DESTROY}  => $objectToDestroy");
-    repo.destroy(objectToDestroy);
-    print("${PropertyChangeConstrains.AFTER_DESTROY}  => $objectToDestroy");
-    return objectToDestroy;
-  }
+  Domain destroy(Domain objectToDestroy) => repo.destroy(objectToDestroy);
 
+  @override
+  int count() => repo.count();
+
+  @override
   void init() {}
 
-  void dispose() {}
-
   @override
-  int count() {
-    return repo.count();
-  }
+  void dispose() {}
 }
